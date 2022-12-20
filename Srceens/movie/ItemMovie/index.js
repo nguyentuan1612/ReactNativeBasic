af8ee4ec1849d7e0514d5559aef249ad5f85ecd6
Nguyen Tuan <tuanbeo203@gmail.com>
2022-12-20 19:25:48 +0700

    ReactNativeBasic

---
+++ Srceens/movie/ItemMovie/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import styles from './style'
+const ItemMovie = () => {
+  return (
+    <View>
+      <Text>ItemMovie</Text>
+    </View>
+  )
+}
+
+export default ItemMovie
