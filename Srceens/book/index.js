fcb8af1d682433237b8ac6ea476bee44bca39098
Nguyen Tuan <tuanbeo203@gmail.com>
2022-12-21 10:21:36 +0700

    new change

--- Srceens/book/index.js
+++ Srceens/book/index.js
@@ -5,12 +5,15 @@ export default function Book() {
   return (
     <View style={styles.container}>
       <ImageBackground
-        source={{uri:"https://r1.ilikewallpaper.net/iphone-12-pro-max-wallpapers/download-149521/book-open-on-dried-leaves-top-view-photography.jpg"}}
-        style={{width: "100%", height: "100%"}}
+        source={{
+          uri: "https://r1.ilikewallpaper.net/iphone-12-pro-max-wallpapers/download-149521/book-open-on-dried-leaves-top-view-photography.jpg",
+        }}
+        style={{ width: "100%", height: "100%" }}
         blurRadius={5}
-     >
-
-
+      >
+        <View style={styles.body}>
+          <Text style={styles.textHeader}>Expor Market</Text>
+        </View>
       </ImageBackground>
     </View>
   );
@@ -19,5 +22,17 @@ export default function Book() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+
+  body: {
+    padding: 12,
+
+  },
+
+  textHeader: {
+    fontWeight: "bold",
+    fontSize: 20,
+    color: "white",
+    marginTop: 20,
   },
 });
