40363cc6f1c43203898f56be594465fa07754b3a
Nguyen Tuan <tuanbeo203@gmail.com>
2022-12-21 10:11:01 +0700

    new changes

--- Srceens/book/index.js
+++ Srceens/book/index.js
@@ -1,10 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
 
 export default function Book() {
   return (
     <View style={styles.container}>
-      
+      <ImageBackground
+        source={{uri:"https://r1.ilikewallpaper.net/iphone-12-pro-max-wallpapers/download-149521/book-open-on-dried-leaves-top-view-photography.jpg"}}
+        style={{width: "100%", height: "100%"}}
+        blurRadius={5}
+     >
+
+
+      </ImageBackground>
     </View>
   );
 }
@@ -12,6 +19,5 @@ export default function Book() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-   
   },
 });
